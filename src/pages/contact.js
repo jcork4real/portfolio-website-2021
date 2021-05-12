d69a84303fe9867038e63dbd55f869dc2c805030
Jarone McCorkle <60796768+jcork4real@users.noreply.github.com>
2021-05-12 02:40:05 -0600

    Uploaded pages and components

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from "../pages/components/layout"
+
+
+const ContactPage = () => {
+    return(
+        <div>
+            <Layout>
+            <h1>Contact</h1>
+            <p>The best way to reach me is to DM on LinkedIn.</p>
+            </Layout>
+        </div>
+    )
+}
+
+export default ContactPage;
